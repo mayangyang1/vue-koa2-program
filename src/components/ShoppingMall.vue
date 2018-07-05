01b147a5f519d406b7615a8de46eccbc3d3ca675
mayangyang <135878>
2018-07-05 16:56:11 +0800

    first commit

---
+++ src/components/ShoppingMall.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <van-row class="search-bar">
+            <van-col class="search-icon" span="3">
+                <img width="80%" :src="locationIcon" alt="">
+            </van-col>
+            <van-col class="serach-input" span="16">
+                <input placeholder="查找" type="text">
+            </van-col>
+            <van-col span="5">
+                <van-button size="mini">查找</van-button>
+            </van-col>
+        </van-row>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                locationIcon: require('../assets/images/location.png'),
+            }
+        }
+    }
+</script>
+
+<style scoped>
+.search-bar{
+    height: 2.2rem;
+    line-height: 2.2rem;
+    background-color: #e5017d;
+}
+.search-icon{
+    padding-top: .2rem;
+    padding-left: .3rem;
+}
+.serach-input input{
+    width: 100%;
+    border: none;
+    height: 1.3rem;
+    border-bottom: 1px solid #fff !important;
+    background-color: #e5017d;
+    color: #fff;
+    font-size: .6rem;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import ShoppingMall from '@/components/ShoppingMall'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {path: '/', name: 'ShoppingMall', component: ShoppingMall}
+  ]
+})
