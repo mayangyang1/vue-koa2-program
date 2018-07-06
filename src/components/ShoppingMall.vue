504328952a9f3ad6a074e5d2fed63c0d6b44aa14
mayangyang <135878>
2018-07-06 09:32:19 +0800

    in shoppingMall new add type-banner modules an anBanner modules

--- src/components/ShoppingMall.vue
+++ src/components/ShoppingMall.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="container">
         <div class="search-bar">
             <van-row>
                 <van-col class="search-icon" span="3">
@@ -15,37 +15,66 @@
         </div>
         <div class="swiper-area">
             <van-swipe :autoplay="3000">
-                <van-swipe-item v-for="(banner, index) in bannerPicArray" :key="index" v-lazy="banner.imageUrl">
-                    <img :src="banner.imageUrl" width="100%" alt="">
+                <van-swipe-item v-for="(banner, index) in bannerPicArray" :key="index" >
+                    <img v-lazy="banner.image" width="100%" alt="">
                 </van-swipe-item>
             </van-swipe>
+        </div>
+        <div class="type-bar">
+            <div v-for="(cate, index) in category" :key="index">
+                <img v-lazy="cate.image" width="90%" alt="">
+                <span>{{cate.mallCategoryName}}</span>
+            </div>
+        </div>
+        <div class="adBanner">
+            <img v-lazy="adBannerUrl" width="100%" alt="">
         </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
     export default {
         data() {
             return {
                 locationIcon: require('../assets/images/location.png'),
-                bannerPicArray: [
-                    {imageUrl: 'http://7xjyw1.com1.z0.glb.clouddn.com/simleVueDemoPic001.jpg'},
-                    {imageUrl: 'http://7xjyw1.com1.z0.glb.clouddn.com/simleVueDemoPic002.jpg'},
-                    {imageUrl: 'http://7xjyw1.com1.z0.glb.clouddn.com/simleVueDemoPic003.jpg'}
-                ]
+                category:[],
+                bannerPicArray: [],
+                adBannerUrl: ''
             }
+        },
+        methods:{
+            getShoppingMallData() { //获取商品首页数据
+                const that = this;
+                axios({
+                    url: 'https://www.easy-mock.com/mock/5b3deb59b4180c1de2924a98/example/shoppingMall/index',
+                    method: 'get'
+                }).then(response => {
+                    console.log(response);
+                    var shoppingMall = response.data.data;
+                    that.category = shoppingMall.category;
+                    that.bannerPicArray = shoppingMall.slides;
+                    that.adBannerUrl = shoppingMall.advertesPicture.PICTURE_ADDRESS
+                }).catch(error => {
+                    console.log(error);
+                })
+            }
+        },
+        created() {
+            this.getShoppingMallData();
         }
     }
 </script>
 
 <style scoped>
+.container{
+    background-color: #f0f0f0;
+}
 .search-bar{
     height: 2.2rem;
     line-height: 2.2rem;
     background-color: #e5017d;
-}
-.search-icon{
-    line-height: 1.2rem;
+    overflow: hidden;
 }
 .search-icon img{
     margin-top: .2rem;
@@ -64,5 +93,21 @@
     width: 20rem;
     overflow: hidden;
     clear: both;
+    max-height: 10rem;
+
+}
+.type-bar{
+    display:flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    font-size: 12px;
+    background-color: #fff;
+    border-radius: .3rem;
+    margin:0 .3rem .3rem .3rem;
+}
+.type-bar div{
+    padding: .4rem;
+    text-align: center;
+    flex: 1;
 }
 </style>
